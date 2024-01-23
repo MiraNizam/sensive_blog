3088ae53d87f6c0aec87abf7ab7fca5b45b5ccad
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2024-01-23 21:33:53 +0300

    Applied Django coding style

--- blog/models.py
+++ blog/models.py
@@ -51,18 +51,18 @@ class Post(models.Model):
         related_name="posts",
         verbose_name="Теги")
 
-    def __str__(self):
-        return self.title
-
     objects = PostQuerySet.as_manager()
-
-    def get_absolute_url(self):
-        return reverse("post_detail", args={"slug": self.slug})
 
     class Meta:
         ordering = ["-published_at"]
         verbose_name = "пост"
         verbose_name_plural = "посты"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args={"slug": self.slug})
 
 
 class TagQuerySet(models.QuerySet):
@@ -82,21 +82,21 @@ class TagQuerySet(models.QuerySet):
 class Tag(models.Model):
     title = models.CharField("Тег", max_length=20, unique=True)
 
-    def __str__(self):
-        return self.title
-
-    def clean(self):
-        self.title = self.title.lower()
-
-    def get_absolute_url(self):
-        return reverse("tag_filter", args={"tag_title": self.slug})
+    objects = TagQuerySet.as_manager()
 
     class Meta:
         ordering = ["title"]
         verbose_name = "тег"
         verbose_name_plural = "теги"
 
-    objects = TagQuerySet.as_manager()
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("tag_filter", args={"tag_title": self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
 
 
 class Comment(models.Model):
@@ -113,10 +113,10 @@ class Comment(models.Model):
     text = models.TextField("Текст комментария")
     published_at = models.DateTimeField("Дата и время публикации")
 
-    def __str__(self):
-        return f"{self.author.username} under {self.post.title}"
-
     class Meta:
         ordering = ["published_at"]
         verbose_name = "комментарий"
         verbose_name_plural = "комментарии"
+
+    def __str__(self):
+        return f"{self.author.username} under {self.post.title}"
